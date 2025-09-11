78fdb0e942107f022db8ea6df42871bc5aedda42
nando2122 <caitanofernando7@gmail.com>
2025-09-11 00:05:13 +0000

    fiz algumas alterações em valores, logo depois identei if e if-else nas linhas dos atributos e com com isso obtive o resultado que queria de mostrar quem venceu

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,20 +1,120 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+   
+    int cartaA, cartaB;
 
+    int pontoscartaA = 70;
+    int pontoscartaB = 80;
+
+    int populacaocartaA = 300000;
+    int populacaocartaB = 50000;
+
+    float areacartaA = 5.000000;
+    float areacartaB = 3.900000;
+
+    float pibcartaA = 5.0000000;
+    float pibcartaB = 4.9000000;
+
+    char estadocartaA [10] = "R";
+    char estadocartaB [10] = "B";
+
+    char cidadecartaA [20] = "Rio de Janeiro";
+    char cidadecartaB [20] = "Bahia";
+
+    char codigocartaA [20] = "R01";
+    char codigocartaB [20] = "B01";
+
+    float densidadecartaA, densidadecartaB;
+    float pibcapitacartaA, pibcapitacartaB;
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    float somaA, somaB;
 
+    printf("***CartaA e CartaB***\n");
+
+    printf("Digite o Estado da cartaA: %s\n", estadocartaA);
+    printf("Digite o Estado da cartaB: %s\n", estadocartaB);
+
+    printf("Digite a cidade da cartaA: %s\n", cidadecartaA);
+    printf("Digite a cidade da cartaB: %s\n", cidadecartaB);
+
+    printf("Digite o código da cartaA: %s\n", codigocartaA);
+    printf("Digite o código da cartaB: %s\n", codigocartaB);
+
+    printf("***Comparação da Carta A e Carta B***\n");
+
+    printf("Digite quantos pontos turísticos tem a cartaA: %d\n", pontoscartaA);
+    printf("Digite quantos pontos turísticos tem a cartaB: %d\n", pontoscartaB);
+
+    if(pontoscartaA > pontoscartaB){
+        printf("Carta A (Rio de Janeiro) venceu!\n");
+    
+    }else{
+        printf("Carta B (Bahia) venceu!\n");
+
+    }
+    
+    printf("Digite o número da população da cartaA: %d mil\n", populacaocartaA);
+    printf("Digite o número da população da cartaB: %d mil\n", populacaocartaB);
+     
+    if(populacaocartaA > populacaocartaB){
+        printf("Carta A (Rio de Janeiro) venceu!\n");
+
+    }else{
+        printf("Carta B (Bahia) venceu!\n");
+
+    }
+
+    printf("Digite quanto tem de área em km² a cartaA: %f milhões\n", areacartaA);
+    printf("Digite quanto tem de área em km² a cartaB: %f milhões\n", areacartaB);
+
+    if(areacartaA > areacartaB){
+        printf("Carta A (Rio de Janeiro) venceu!\n");
+
+    }else{
+        printf("Carta B (Bahia) venceu!\n");
+
+    }
+
+    printf("Digite o número do PIB da cartaA: %f milhões\n", pibcartaA);
+    printf("Digite o número do PIB da cartaB: %f milhões\n", pibcartaB);
+    
+    if(pibcartaA > pibcartaB){
+        printf("Carta A (Rio de Janeiro) venceu!\n");
+
+    }else{
+        printf("Carta B (Bahia) venceu!\n");
+
+    }
+
+    densidadecartaA = populacaocartaA / areacartaA;
+    densidadecartaB = populacaocartaB / areacartaB;
+
+    pibcapitacartaA = populacaocartaA / pibcartaA;
+    pibcapitacartaB = populacaocartaB / pibcartaB;
+
+    printf("Densidade populacional da cartaA é: %.2f hab/km²\n", densidadecartaA);
+    printf("Densidade populacional da cartaB é: %.2f hab/km²\n", densidadecartaB);
+
+    if(densidadecartaA < densidadecartaB){
+        printf("Carta A (Rio de Janeiro) venceu!\n");
+
+    }else{
+        printf("Carta B (Bahia) venceu!\n");
+
+    }
+
+    printf("PIB per capita: %.2f reais\n", pibcapitacartaA);
+    printf("PIB per capita: %.2f reais\n", pibcapitacartaB);
+
+    if(pibcapitacartaA > pibcapitacartaB){
+        printf("Carta A (Rio de Janeiro) venceu!\n");
+
+    }else{
+        printf("Carta B (Bahia) venceu!\n");
+
+    }
+    
     // Exemplo:
     // printf("Digite o código da cidade: ");
     // scanf("%s", codigo);
